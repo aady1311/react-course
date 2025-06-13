1f4c46dca0ba56c7c9e2e262e8bc37a6db7998ea
Adity Yadav <aadityayadav7656@gmail.com>
2025-06-13 10:49:06 +0530

    Basics how to create routing and use

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,11 @@
+import Youtube from "./Youtube";
+function App() {
+  return (
+    <div className="App">
+    <h1>Classical react DOM</h1>
+    <Youtube/>
+    </div>
+  );
+}
+
+export default App;
